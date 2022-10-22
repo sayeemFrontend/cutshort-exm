7ed0be20741a3cf9a9b132b6a21959b1e70cee6f
unknown <amanullahsayeem@gmail.com>
2022-10-22 16:20:20 +0600

    fix: remove listener to stop unnecessary rendering

--- src/components/common/modal/Modal.js
+++ src/components/common/modal/Modal.js
@@ -2,13 +2,17 @@ import React, { useEffect } from "react";
 import ReactDOM from "react-dom";
 
 export default function Modal({ children = null, onClose = () => {} }) {
+  const handleResize = () => {
+    if (document.documentElement.clientWidth > 1023) {
+      onClose();
+    }
+  };
+
   useEffect(() => {
-    window.addEventListener("resize", () => {
-      if (document.documentElement.clientWidth > 1023) {
-        onClose();
-      }
-    });
-  });
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
   const portal = (
     <div className="modal bg-white z-50 w-screen h-screen fixed left-0 top-0">
       <div>{children}</div>
